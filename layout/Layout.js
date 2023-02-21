21a9a87414e2ce9badb2fe8fca0d55863059ebac
RyoLinEth <ryolindev@gmail.com>
2023-02-21 18:36:30 +0800

    first commit

---
+++ layout/Layout.js
@@ -0,0 +1,56 @@
+import Head from "next/head";
+import { Fragment, useEffect } from "react";
+import ImageView from "../components/popup/ImageView";
+import VideoPopup from "../components/popup/VideoPopup";
+import { dataBgImg, holdSection, imgToSVG } from "../utilits";
+import Footer from "./Footer";
+import Header from "./Header";
+import MobileNavigation from "./MobileNavigation";
+import Navigation from "./Navigation";
+import PreLoader from "./PreLoader";
+import ScrollTop from "./ScrollTop";
+import Searchbox from "./Searchbox";
+import SearchButton from "./SearchButton";
+import Social from "./Social";
+import WalletPopUp from "./WalletPopUp";
+const Layout = ({ children, pageTitle }) => {
+  useEffect(() => {
+    holdSection();
+    imgToSVG();
+    dataBgImg();
+  }, []);
+
+  return (
+    <Fragment>
+      <Head>
+        <title>MetaPortal | {pageTitle}</title>
+      </Head>
+      <ImageView />
+      <VideoPopup />
+      <PreLoader />
+      {/* !Preloader */}
+      {/* Left Navigation */}
+      <Navigation />
+      {/* !Left Navigation */}
+      {/* Searchbox Popup */}
+      <Searchbox />
+      {/* !Searchbox Popup */}
+      {/* Wallet Popup */}
+      <WalletPopUp />
+      {/* !Wallet Popup */}
+      {/* Main */}
+      <div className="metaportal_fn_main">
+        <MobileNavigation />
+        <Header />
+        <div className="metaportal_fn_content">
+          {children}
+          <Footer />
+        </div>
+        <Social />
+        <ScrollTop />
+        <SearchButton />
+      </div>
+    </Fragment>
+  );
+};
+export default Layout;
